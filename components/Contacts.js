0f980f71d0ab8886978e09b7bc0a8962fb92b43f
Radosław Biesek <radoslaw.biesek@gmail.com>
2019-01-22 19:40:00 +0100

    Clean the code

--- components/Contacts.js
+++ components/Contacts.js
@@ -4,16 +4,11 @@ var Contacts = React.createClass({
     },
 
     render: function() {
-        var contacts = this.props.items.map(function(contact){
-            return React.createElement(Contact, {
-                item: contact,
-                key: contact.id
-            })
-        });
-        
         return (
             <ul className='contactsList'>
-                {contacts}
+                {this.props.items.map(function(contact) {
+                    return (<Contact item={contact} key={contact.id} />);
+                })}
             </ul>
         );
     }
